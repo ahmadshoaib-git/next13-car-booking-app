466df417bcdefc49a80e8ed3f26cfe142e468aee
ahmadshoaib-git <ahmad.shoaib88@gmail.com>
2023-10-19 16:53:16 +0500

    chore: react particles added to signin form.

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,11 +1,22 @@
+import Image from 'next/image';
 import SignInForm from '@/components/modules/Form/signIn';
+import Particle from '@/components/modules/Particles/index';
 
 const SignIn = () => {
     return (
-        <main className="w-[100%] h-[100vh] flex justify-center items-center">
-            <div className="shadow-sm border rounded-lg max-w-[20rem] w-full p-[1.5rem]">
-                <h2 className="text-center uppercase pb-[1rem]">Sign In</h2>
-                <SignInForm />
+        <main className="relative w-[100%] h-[100vh] grid grid-cols-2 items-center">
+            {/* <div className="absolute right-0 top-0 h-full w-full z-[1]"> */}
+            <div className="relative h-full">
+                <Image src={'/bg-dark.jpg'} alt="Metaverse" fill className={'!absolute h-full'} />
+                <div className="absolute z-[3]">
+                    <Particle />
+                </div>
+            </div>
+            <div className="h-full w-full flex items-center justify-center">
+                <div className="shadow-sm border rounded-lg max-w-[20rem] w-full p-[1.5rem] z-[2] bg-slate-100/[.6]">
+                    <h2 className="text-center uppercase pb-[1rem]">Sign In</h2>
+                    <SignInForm />
+                </div>
             </div>
         </main>
     );
