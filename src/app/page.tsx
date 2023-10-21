abc8243172fd87ebb66505974f4f4946c8c03835
ahmadshoaib-git <ahmad.shoaib88@gmail.com>
2023-10-21 15:48:28 +0500

    feat: data from db brought and shown via prisma.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
-    return <main className="flex min-h-screen flex-col items-center p-24 gap-[1rem]">Hello World</main>;
+import { prisma } from '../../lib/prisma';
+
+export default async function Home() {
+    const user = await prisma.user.findFirst({
+        where: {
+            email: 'abc@yopmail.com',
+        },
+    });
+
+    return <main className="flex min-h-screen flex-col items-center p-24 gap-[1rem]">Hello {`${user?.firstName} ${user?.lastName}`}</main>;
 }
 
